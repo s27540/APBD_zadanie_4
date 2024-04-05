badc2408c3c62448b2763e225b2f4734aa52ec28
s27540 <s27540@pjwstk.edu.pl>
2024-04-05 23:33:49 +0200

    Animal class and DB add

--- WebApplication1/WebApplication1/DB/DB.cs
+++ WebApplication1/WebApplication1/DB/DB.cs
@@ -1,6 +1,15 @@
-﻿namespace WebApplication1.DB;
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.DB;
 
 public class DB
 {
-    
+    private List<Animal> animals = new()
+    {
+        new Animal { Id = 1, Name = "Lili", Category = "Dog", CoatColor = "White", Mass = 23.5},
+        new Animal { Id = 2, Name = "Johnathan", Category = "Dog", CoatColor = "Black", Mass = 43.6},
+        new Animal { Id = 3, Name = "Viviane", Category = "Cat", CoatColor = "Red and White", Mass = 15.4},
+        new Animal { Id = 4, Name = "Bob", Category = "Turtle", CoatColor = "Sand", Mass = 65.8},
+        new Animal { Id = 5, Name = "Colt", Category = "Fox", CoatColor = "Red", Mass = 46.3}
+    };
 }
--- WebApplication1/WebApplication1/Models/Animal.cs
+++ WebApplication1/WebApplication1/Models/Animal.cs
@@ -2,5 +2,9 @@
 
 public class Animal
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public double Mass { get; set; }
+    public string CoatColor { get; set; }
 }
